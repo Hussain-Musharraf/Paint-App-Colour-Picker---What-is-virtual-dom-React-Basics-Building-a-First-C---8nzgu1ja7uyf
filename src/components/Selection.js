b0cbbec5deb0825ffd5d93138d4722d26e15f3b8
Musharraf8543 <musharrafhussain4397@gmail.com>
2022-11-12 04:17:44 +0530

    coddedd

--- src/components/Selection.js
+++ src/components/Selection.js
@@ -1,1 +1,12 @@
+import React from "react";
+import { useState } from "react";
 
+const Selection=(props)=>{
+    const [getBackground,setBackground]=useState({background:''})
+    return(
+        <div className="fix-box" onClick={()=>props.applyColor(setBackground)} style={getBackground}>
+            <h2 className="subheading">Selection</h2>
+        </div>
+    )
+}
+export default Selection;
