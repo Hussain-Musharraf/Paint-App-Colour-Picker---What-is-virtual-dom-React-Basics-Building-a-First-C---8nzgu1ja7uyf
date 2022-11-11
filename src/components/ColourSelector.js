b4fc3cac54f0d9239d9bc608c63349b185dcc7e8
Musharraf8543 <musharrafhussain4397@gmail.com>
2022-11-12 04:59:38 +0530

    addedsuccessfully

--- src/components/ColourSelector.js
+++ src/components/ColourSelector.js
@@ -10,3 +10,14 @@ const ColourSelector = (props) => {
   )
 }
 export default ColourSelector;
+
+
+// import React from ‘react’;
+// const ColourSelector = (props) => {
+//   const { config, selectNextBackground } = props
+//   const {label, classname, background } = config;
+//   return (
+//     <button className={classname} onClick={() => selectNextBackground({background: background})}>{label}</button>
+//   )
+// }
+// export default ColourSelector;
--- src/components/Selection.js
+++ src/components/Selection.js
@@ -10,3 +10,15 @@ const Selection=(props)=>{
     )
 }
 export default Selection;
+
+// import React,{ useState } from ‘react’
+// import ‘../styles/Selection.css’
+// const Selection = (props) => {
+//     const [selectionStyle, updateSelectionStyle] = useState({  })
+//     return (
+//         <div className=“fix-box” style={selectionStyle} onClick={() => {props.applyColor(updateSelectionStyle)}}>
+//             <h2 className=‘subheading’>Selection</h2>
+//         </div >
+//     )
+// }
+// export default Selection;
